78962f32777186fcb04c886b7b0a70cd69adc4cc
Yul310 <hsy3102@gmail.com>
2022-12-13 21:53:02 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-var figlet = require('figlet');
+// var figlet = require('figlet');
 
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+// figlet('Hello World!!', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
+
+
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/dog', function (req, res) {
+    res.send('dog')
+})
+
+app.get('/cat', function (req, res) {
+    res.send('cat')
+})
+
+app.get('/user/:id', function (req, res) {
+    const q = req.params
+    console.log(q)
+})
+app.get('/user/:id', function (req, res) {
+    const q = req.params
+    console.log(q)
+})
+
+app.listen(3000)
